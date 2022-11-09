4f21c9b9f78b30735924ad2bf3a7d69d076cfbe2
Leandro Cruz <leandro.programmer@hotmail.com>
2022-11-09 08:59:56 -0300

    chore: Creating a new page and navigating between pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,39 @@
+import Link from 'next/link'
 import { ReactNode } from 'react'
 
 interface RootLayoutProps {
   children: ReactNode
 }
 
+const links = [
+  { label: 'Home', route: '/' },
+  { label: 'About', route: '/about' }
+]
+
 export default function RootLayout ({ children }: RootLayoutProps) {
   return (
     <html>
-      <head />
+      <head>
+        <title>My app with NextJS 13</title>
+      </head>
 
-      <body>{children}</body>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              {links.map(link => (
+                <li key={link.route}>
+                  <Link href={link.route}>
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </header>
+
+        {children}
+      </body>
     </html>
   )
 }
